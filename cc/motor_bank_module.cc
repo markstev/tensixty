440cf6d4df1be96af9b26d80ff7158d1edde286a
Mark Stevens <stevens.mark.a@gmail.com>
2020-05-13 00:04:54 -0700

    Fixed interrupts

--- cc/motor_bank_module.cc
+++ cc/motor_bank_module.cc
@@ -1,34 +1,46 @@
 #include "motor_bank_module.h"
 
 namespace markbot {
+Motor MOTORS[NUM_MOTORS];
+}  // namespace markbot
 
 #if not __x86_64__
+#if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif 
 ISR(TIMER2_COMPA_vect) {
-  for (int i = 0; i < NUM_MOTORS; ++i) {
-    MOTORS[i].FastTick();
+  for (int i = 0; i < markbot::NUM_MOTORS; ++i) {
+    markbot::MOTORS[i].FastTick();
   }
 }
 #endif  // not __x86_64__
 
+namespace markbot {
+
 MotorBankModule::MotorBankModule(tensixty::ArduinoInterface *arduino)
     : arduino_(arduino), request_report_(false) {
+}
+
+void MotorBankModule::Setup() {
 #if not __x86_64__
   cli();//stop interrupts
 
   //set timer2 interrupt at 8kHz
-  TCCR2A = 0;// set entire TCCR0A register to 0
-  TCCR2B = 0;// same for TCCR0B
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
   TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR2A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // set compare match register for 16khz increments
-  OCR2A = 124;// = (16*10^6) / (16000*8) - 1 (must be <256)
+  // set compare match register for 8khz increments
+  // interrupt frequency = 16Mhz / prescale / OCR2A
+  // Note, the physical limits of stepper motors seems to be around 4kHz.
+  OCR2A = 124;  // = (16*10^6) / (4000*32) - 1
   // turn on CTC mode
   TCCR2A |= (1 << WGM21);
-  // Set CS01 and CS00 bits for 64 prescaler
-  //TCCR2B |= (1 << CS21) | (1 << CS20);   
   // Set CS21 bit for 8 prescaler
   TCCR2B |= (1 << CS21);   
+  // Set CS20 bit as well -> 32 prescaler
+  TCCR2B |= (1 << CS20);   
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE2A);
   sei();
