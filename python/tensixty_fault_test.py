5d86cc7f4112b3a699efeb92acf17466898a7b79
Mark Stevens <stevens.mark.a@gmail.com>
2020-07-28 22:00:15 -0700

    Works in half duplex but not full duplex mode with faults. 50% of test runs failing

--- python/tensixty_fault_test.py
+++ python/tensixty_fault_test.py
@@ -144,18 +144,18 @@ class TensixtyFaultTest(unittest.TestCase):
         iterations = 50
         for i in range(iterations):
             device0.SendInts([42, i, 2, 3])
-            # device1.SendInts([42, i, 0, 9, ord('\\'), 9, 0, ord('\n')])
+            device1.SendInts([42, i, 0, 9, ord('\\'), 9, 0, ord('\n')])
             start_time = time.time()
             while time.time() - start_time < 15.0:
                 if len(device1_outputs) > i:
                     self.assertEqual([i, 2, 3], device1_outputs[i])
                     break
             self.assertEqual(i + 1, len(device1_outputs))
-            #   while time.time() - start_time < 15.0:
-            #       if len(device0_outputs) > i:
-            #           self.assertEqual([i, 0, 9, ord('\\'), 9, 0, ord('\n')], device0_outputs[i])
-            #           break
-            #   self.assertEqual(i + 1, len(device0_outputs))
+            while time.time() - start_time < 15.0:
+                if len(device0_outputs) > i:
+                    self.assertEqual([i, 0, 9, ord('\\'), 9, 0, ord('\n')], device0_outputs[i])
+                    break
+            self.assertEqual(i + 1, len(device0_outputs))
 
 if __name__ == '__main__':
         unittest.main()
